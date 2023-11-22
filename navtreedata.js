4d7d11fd565136daebc9a4d673fe038315c97a32
fakemoses <fakemoses@users.noreply.github.com>
2023-11-22 08:54:18 +0000

    deploy: 3e80f6c04177497613e8d012306829e2af0fec5f

--- navtreedata.js
+++ navtreedata.js
@@ -26,46 +26,46 @@ var NAVTREE =
 [
   [ "Maxima Plugin", "index.html", [
     [ "Overview", "index.html", [
-      [ "Contents", "index.html#autotoc_md21", null ],
-      [ "Some Useful Links", "index.html#autotoc_md22", null ]
+      [ "Contents", "index.html#autotoc_md22", null ],
+      [ "Some Useful Links", "index.html#autotoc_md23", null ]
     ] ],
     [ "Getting Started with Development on Linux", "md_documentations_DevGuide_Getting_Started_with_Development_on_Linux.html", null ],
     [ "Project Structure & Examples", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html", [
-      [ "Folder in root directory", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md3", null ],
-      [ "Files in root directory", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md4", null ],
-      [ "Main component of the project", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md5", [
-        [ "1. Entry point", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md6", null ],
-        [ "2. Utilities and Mathematical Functions", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md7", [
-          [ "Utility Functions", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md8", null ],
-          [ "Symbolic alternatives Functions", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md9", null ],
-          [ "Wrapper Functions", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md10", null ]
+      [ "Folder in root directory", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md4", null ],
+      [ "Files in root directory", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md5", null ],
+      [ "Main component of the project", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md6", [
+        [ "1. Entry point", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md7", null ],
+        [ "2. Utilities and Mathematical Functions", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md8", [
+          [ "Utility Functions", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md9", null ],
+          [ "Symbolic alternatives Functions", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md10", null ],
+          [ "Wrapper Functions", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md11", null ]
         ] ],
-        [ "3. Translation Interface", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md11", null ],
-        [ "4. Communication Interface", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md12", null ],
-        [ "5. Draw Region and Draw Functions", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md13", null ],
-        [ "6. Maxima Automatic Installation", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md14", null ]
+        [ "3. Translation Interface", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md12", null ],
+        [ "4. Communication Interface", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md13", null ],
+        [ "5. Draw Region and Draw Functions", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md14", null ],
+        [ "6. Maxima Automatic Installation", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md15", null ]
       ] ],
-      [ "Examples", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md15", [
-        [ "1. Create new SMath Function", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md16", null ],
-        [ "2. Add new Item in Toolbox", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md17", null ],
-        [ "3. Add new Item in Menu Bar", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md18", null ],
-        [ "4. Adding new Plot Options", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md19", null ]
+      [ "Examples", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md16", [
+        [ "1. Create new SMath Function", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md17", null ],
+        [ "2. Add new Item in Toolbox", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md18", null ],
+        [ "3. Add new Item in Menu Bar", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md19", null ],
+        [ "4. Adding new Plot Options", "md_documentations_DevGuide_Getting_Started_with_Development_on_Windows.html#autotoc_md20", null ]
       ] ]
     ] ],
     [ "Getting Started - Developer Guide", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html", [
-      [ "Requirements for development:", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md23", null ],
-      [ "Cloning the codebase", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md24", [
-        [ "Cloning from GitHub", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md25", null ],
-        [ "Cloning from SMath SVN Server", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md26", null ]
+      [ "Requirements for development:", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md24", null ],
+      [ "Cloning the codebase", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md25", [
+        [ "Cloning from GitHub", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md26", null ],
+        [ "Cloning from SMath SVN Server", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md27", null ]
       ] ],
-      [ "Setting up working environment:", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md27", null ],
-      [ "Commiting changes", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md28", null ],
-      [ "NOTE WHEN COMMITTING CHANGES FOR PLUGIN RELEASE", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md29", null ],
-      [ "Releasing the Plugin", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md30", null ]
+      [ "Setting up working environment:", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md28", null ],
+      [ "Commiting changes", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md29", null ],
+      [ "NOTE WHEN COMMITTING CHANGES FOR PLUGIN RELEASE", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md30", null ],
+      [ "Releasing the Plugin", "md_documentations_DevGuide_Requirement___Setting_up_the_development.html#autotoc_md31", null ]
     ] ],
     [ "Maxima Plugin", "md_readme.html", [
-      [ "Getting Started", "md_readme.html#autotoc_md32", null ],
-      [ "Other Usefull Links", "md_readme.html#autotoc_md33", null ]
+      [ "Getting Started", "md_readme.html#autotoc_md33", null ],
+      [ "Other Usefull Links", "md_readme.html#autotoc_md34", null ]
     ] ],
     [ "Packages", "namespaces.html", [
       [ "Package List", "namespaces.html", "namespaces_dup" ]
